b88e9e8cbcec96ee7717d750d2db4cdc144be6df
alà eddin abugrara <alaeddin.abugrara@gmail.com>
2019-02-18 20:01:36 +0100

    Task 1.3: Delete the initial content of the script.js file which was just a placeholder, add new content for pokedex app

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,26 @@
-alert("Hello world!")
+var repository = [
+    {name: 'Bulbasaur',
+    height: 0.7,
+    type: ['Grass','Poison']
+    },
+    {name: 'Charmander',
+    height: 0.6,
+    type: ['Fire']
+    },
+    {name: 'Squirtle',
+    height: 0.5,
+    type: ['Water']
+    },
+    {name: 'Caterpie',
+    height: 0.3,
+    type: ['Bug']
+    },
+    {name: 'Mr. Mime',
+    height: 1.3,
+    type: ['Psychic','Fairy']
+    },
+    {name: 'Snorlax',
+    height: 2.1,
+    type: ['Normal']
+    }
+];
