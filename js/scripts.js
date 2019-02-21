07849dee2cf76d8d1113a9457cdd0c6986b87565
alà eddin abugrara <alaeddin.abugrara@gmail.com>
2019-02-21 19:10:04 +0100

    Task 1.4 wrap the whole code inside an IFEE to avoid accessing global variables

--- js/scripts.js
+++ js/scripts.js
@@ -1,36 +1,38 @@
-var repository = [
-    {name: 'Bulbasaur',
-    height: 0.7,
-    type: ['Grass','Poison']
-    },
-    {name: 'Charmander',
-    height: 0.6,
-    type: ['Fire']
-    },
-    {name: 'Squirtle',
-    height: 0.5,
-    type: ['Water']
-    },
-    {name: 'Caterpie',
-    height: 0.3,
-    type: ['Bug']
-    },
-    {name: 'Mr. Mime',
-    height: 1.3,
-    type: ['Psychic','Fairy']
-    },
-    {name: 'Snorlax',
-    height: 2.1,
-    type: ['Normal']
+(function(){
+    var repository = [
+        {name: 'Bulbasaur',
+        height: 0.7,
+        type: ['Grass','Poison']
+        },
+        {name: 'Charmander',
+        height: 0.6,
+        type: ['Fire']
+        },
+        {name: 'Squirtle',
+        height: 0.5,
+        type: ['Water']
+        },
+        {name: 'Caterpie',
+        height: 0.3,
+        type: ['Bug']
+        },
+        {name: 'Mr. Mime',
+        height: 1.3,
+        type: ['Psychic','Fairy']
+        },
+        {name: 'Snorlax',
+        height: 2.1,
+        type: ['Normal']
+        }
+    ];
+    
+    for(var i = 0 ; i < repository.length ; i++ ){
+        if(repository[i].height > 2){
+            document.write('Pokemon: ' + repository[i].name + ', ' + 'Type: ' + repository[i].type + ', ' +'Height: ' + repository[i].height + 'Meters' + " Wow, that’s big!" + '<br>')
+        } else if (repository[i].type.length > 1) {
+            document.write('Pokemon: ' + repository[i].name + ', ' + 'Type: ' + repository[i].type[0] + ', ' + repository[i].type[1] + ', '+'Height: ' + repository[i].height + 'Meters' + '<br>')
+        } else {
+            document.write('Pokemon: ' + repository[i].name + ', ' + 'Type: ' + repository[i].type + ', ' +'Height: ' + repository[i].height + 'Meters' + '<br>')
+        }
     }
-];
-
-for(var i = 0 ; i < repository.length ; i++ ){
-    if(repository[i].height > 2){
-        document.write('Pokemon: ' + repository[i].name + ', ' + 'Type: ' + repository[i].type + ', ' +'Height: ' + repository[i].height + 'Meters' + " Wow, that’s big!" + '<br>')
-    } else if (repository[i].type.length > 1) {
-        document.write('Pokemon: ' + repository[i].name + ', ' + 'Type: ' + repository[i].type[0] + ', ' + repository[i].type[1] + ', '+'Height: ' + repository[i].height + 'Meters' + '<br>')
-    } else {
-        document.write('Pokemon: ' + repository[i].name + ', ' + 'Type: ' + repository[i].type + ', ' +'Height: ' + repository[i].height + 'Meters' + '<br>')
-    }
-}
+}) ();
